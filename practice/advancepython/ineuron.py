34723c4f21e49a64c17ddec4f9f80a7d4800165b
Kalpesh1709 <105791501+Kalpesh1709@users.noreply.github.com>
2024-12-30 12:01:39 +0530

    Add files via upload

---
+++ practice/advancepython/ineuron.py
@@ -0,0 +1,5 @@
+data={"name":"kalpesh","course":["dl","ml","python"],"greeting":"hello all"}
+def get_Course():
+    return data["course"]
+def greeting():
+    return data["greeting"]
---
+++ practice/advancepython/test2.py
@@ -0,0 +1,11 @@
+def add_1(a,b):
+    return a+b
+def Sub_1(a,b):
+    return a-b
+def Mul_1(a,b):
+    return a*b
+def Div_1(a,b):
+    """hvshdchcdbjndckdsnkdscndcdhcubcdhveydbcwns,zlcwhdbwchhcd
+    dcjbcwhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhue
+    cbwwwwwwwww"""
+    return a/b
